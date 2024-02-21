13a52bd6d21ba02b64370656ea4c5151bb24c66c
wu67 <wk7113@gmail.com>
2024-02-21 16:56:03 +0800

    init

---
+++ app.js
@@ -0,0 +1,129 @@
+const axios = require('axios')
+const dayjs = require('dayjs')
+
+const config = {
+  regeExp: /\w+/,
+  rpcAddr: 'http://127.0.0.1:6800/jsonrpc', // aria2 rpc 地址
+  secret: '123456', // aria2 rpc 密码
+  block_keywords: ['XL', 'SD', 'XF', 'QD', 'BN', 'DL'], // 要ban掉的客户端peer简称
+  interval: 10000,
+}
+// 其实依赖redis是最好的, 但是用来ban ip有点大材小用了, 就干脆用数组凑合, pm2每隔一个小时重启
+const blockList = []
+
+function decodePeerID(str) {
+  // -qb1234-123456789012, peerid 总长20, 正规bt客户端以-(两位简称)(4位版本号)-开头, 其余无意义字符补齐20位,
+  // 在aria2中会被编码过再返回, 其中-被编码为%2D, 退N步讲, 前8位会被编码为12位, 后12位极端情况下为纯数字不必编码, 最少为12位,
+  // 故解码前的数据, 理论合法值不可能小于24位
+  if (!str || str.length < 24) return ''
+
+  // 所以不必解码所有peer id, 这操作本身无意义, 解码前12位即可
+  return str.substring(0, 12).replace(/%2(d|D)/g, '-')
+}
+
+async function asyncProcessingEachItem(list, cb) {
+  const length = list.length
+  for (let i = 0; i < length; i++) {
+    await cb(list[i])
+  }
+}
+
+async function asyncBlockIP(ip, ua) {
+  try {
+    if (blockList.includes(ip)) {
+      return
+    }
+
+    console.log(
+      dayjs().format('YYYY-MM-DD HH:mm:ss') + ' block vampire: ',
+      ip,
+      ` ${ua}`
+    )
+    blockList.push(ip)
+
+    if (ip.includes(':')) {
+      await require('util').promisify(require('child_process').exec)(
+        `ipset add vampire_v6 ${ip}`
+      )
+    } else {
+      await require('util').promisify(require('child_process').exec)(
+        `ipset add vampire_v4 ${ip}`
+      )
+    }
+  } catch (e) {
+    if (
+      typeof e.stderr === 'string' &&
+      e.stderr.indexOf('already added') !== -1
+    ) {
+      // do nothing
+    } else {
+      throw e
+    }
+  }
+}
+
+let isProcessing = false
+async function cron() {
+  isProcessing = true
+  try {
+    const activeTaskList = await axios.post(config.rpcAddr, {
+      jsonrpc: '2.0',
+      method: 'aria2.tellActive',
+      id: `${Math.random()}`,
+      params: [`token:${config.secret}`, ['gid', 'status']],
+    })
+
+    await asyncProcessingEachItem(activeTaskList.data.result, async (task) => {
+      if (task.status !== 'active') return
+
+      let peerList = await axios.post(config.rpcAddr, {
+        jsonrpc: '2.0',
+        method: 'system.multicall',
+        id: `${Math.random()}`,
+        params: [
+          [
+            {
+              methodName: 'aria2.getPeers',
+              params: [`token:${config.secret}`, task.gid],
+            },
+          ],
+        ],
+      })
+
+      await asyncProcessingEachItem(
+        peerList.data.result[0][0],
+        async (peer) => {
+          const ua = decodePeerID(peer.peerId)
+          // console.log(ua, 'ua')
+          // 有些奇葩设置peerid为奇奇怪怪的串, 直接ban掉, 很明显搞事的
+          if (
+            ua === '' ||
+            !/^%2(d|D)/.test(peer.peerId) ||
+            /^(%00%00)+/.test(peer.peerId)
+          ) {
+            await asyncBlockIP(peer.ip, peer.peerId)
+          } else if (config.regExp.test(ua)) {
+            await asyncBlockIP(peer.ip, ua)
+          }
+        }
+      )
+    })
+  } catch (e) {
+    console.error(e)
+  }
+  isProcessing = false
+}
+
+;(function () {
+  console.log(
+    dayjs().format('YYYY-MM-DD HH:mm:ss') + " Let's block vampire now"
+  )
+  config.regExp = new RegExp('-(' + config.block_keywords.join('|') + ')')
+
+  setInterval(() => {
+    if (!isProcessing) {
+      cron()
+    }
+  }, config.interval)
+  cron()
+})()
---
+++ readme.md
@@ -0,0 +1,63 @@
+# Block BT vampire
+
+Block BT vampire by ipset iptables/ip6tables, just for `aria2 rpc mode`
+
+通过 ipset iptables/ip6tables 屏蔽掉部分 BT 吸血客户端, 仅适用于 `aria2 rpc 模式`
+
+## 使用
+
+- config rpc secret in app.js #line 7 , secret should same with your aria2.conf file, like: rpc-secret=balabala, you should type `balabala` in line#7
+- install ipset iptable ip6table
+- install node.js
+- config a base rule of iptables & ipset
+- run with pm2
+
+先更改 app.js 第 7 行的 aria2 密码, 如果 aria2 运行在非 6800 端口, 注意修改第 6 行的 rpc 地址.
+
+接下来以 Debian 为例:
+
+1. 安装 ipset
+
+```bash
+apt install ipset
+```
+
+2. 安装 iptable
+
+```bash
+apt install iptables ip6tables
+```
+
+3. 创建 ipset 集合, 3600 是一小时, 即自动过期时间
+
+```bash
+ipset create vampire_v4 hash:ip timeout 3600
+
+ipset create vampire_v6 hash:ip family inet6 timeout 3600
+```
+
+4. iptable 封禁对应集合的 ip, 这里只封了高位端口, 避免把自己 IP 给封了导致无法 ssh 登录服务器.(ipv4 耗尽的地区的网络, 会有多人共用公共出口的 ipv4, 很可能你的邻居就在用吸血客户端...)
+
+```bash
+iptables -I INPUT -j DROP -p tcp --dport 9999:65535 -m set --match-set vampire_v4 src
+
+ip6tables -I INPUT -j DROP -p tcp --dport 9999:65535 -m set --match-set vampire_v6 src
+```
+
+注: 这样配置在机器重启后就会丢失. 我是用 systemctl 启动了开机初始化服务, 这里仅介绍了如果用最简单的方法跑通, 数据持久化请自行处理.
+
+5. 安装 node.js
+
+自行处理 [https://nodejs.org/en/download/](https://nodejs.org/en/download/)
+
+6. 安装 pm2 并启动
+
+```bash
+# 先cd到对应目录
+
+# 然后安装pm2
+npm i -g pm2
+
+# pm2 启动, 并设置每小时重启, 对应ipset里每小时过期的规则
+pm2 start app.js -c "0 * * * *"
+```
